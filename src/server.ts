aa0c45622ec79cb02e086e070a5c78f7ae39154f
kapilPatel <kapildevtech5@gmail.com>
2025-07-14 18:36:57 +0530

    cors fixed

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import "reflect-metadata";
 import { AppDataSource } from "./config/ormconfig";
 import app from "./app";
 import { ENV } from "./config/env";
+import cors from 'cors';
 
 const startServer = async () => {
   try {
     await AppDataSource.initialize();
     console.log("Database connected!");
+
+    app.use(cors({
+      origin: 'http://localhost:3000', // No trailing slash!
+      credentials: true, // If you need to send cookies/auth headers
+    }));
 
     app.listen(ENV.PORT, () => console.log(`Server running on port ${ENV.PORT}`));
   } catch (error) {
